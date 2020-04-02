714c31254b5322e67cf44db17db571677d94dd9b
abachant <gbachant@gmail.com>
2020-04-02 12:39:13 -0400

    Add bootstrap cdn

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Navbar() {
   return(
-    <nav className="navbar navbar-default">
+    <nav className="navbar navbar-dark bg-dark">
       <div className="container-fluid">
         <div className="navbar-header">
-          <a className="navbar-brand" href="index.html">GB</a>
+          <a className="navbar-brand" href="index.html">AB</a>
         </div>
         <ul className="nav navbar-nav">
           <li><a href="about.html">About</a></li>
