9409921aef5bc205ae668d3ef8fb39036aff8d0c
abachant <gbachant@gmail.com>
2020-04-04 15:34:44 -0400

    Add basic Resume component with basic routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import Navbar from './components/layout/navbar.js'
-import About from './components/about.js'
-import Footer from './components/layout/footer.js'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Navbar from './components/layout/navbar.js';
+import About from './components/about.js';
+import Resume from './components/resume.js';
+import Footer from './components/layout/footer.js';
 
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <About />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={About} />
+          <Route path="/resume" component={Resume} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/resume.js
@@ -0,0 +1,113 @@
+import React from 'react';
+
+
+function Resume() {
+  return(
+    <div>
+        <div>
+            <h1>Aubrey Bachant</h1>
+            <h4><a href="mailto:aubreybachant@gmail.com">aubreybachant@gmail.com</a> | 774-400-8759 | <a href="https://www.linkedin.com/in/garrett-bachant-36bb77115/">LinkedIn</a> | <a href="https://github.com/abachant">GitHub</a></h4>
+            <h4>Waltham, Massachusetts <small>(willing to relocate)</small></h4>
+        </div>
+            <hr />
+        <div>
+            <h3>Skills</h3>
+            <ul>
+                <li>Python</li>
+                <ul>
+                    <li>Pandas</li>
+                    <li>Jupyter Notebook</li>
+                    <li>Matplotlib</li>
+                    <li>Plotly</li>
+                </ul>
+                <li>JavaScript</li>
+                <ul>
+                    <li>React</li>
+                    <li>jQuery</li>
+                    <li>Jest</li>
+                </ul>
+                <li>Firebase</li>
+                <li>Git/GitHub</li>
+                <li>Bash</li>
+                <li>HTML</li>
+                <ul>
+                    <li>Bootstrap</li>
+                </ul>
+                <li>CSS</li>
+                <ul>
+                    <li>SCSS/SASS</li>
+                </ul>
+                <li>Tulip App Builder</li>
+                <li>Windows Office(Word, Excel, Powerpoint)</li>
+                <li>SketchUp CAD Software</li>
+                <li>French(intermediate)</li>
+                <li>Reception</li>
+                <li>Construction/Carpentry/Metalworking</li>
+            </ul>
+            <h3>Projects</h3>
+                <h4>In Progress</h4>
+                <h5><a href="https://github.com/Gbachant/Lane-Claim">Lane Claim</a></h5>
+                    <ul>
+                    <li>Lane Claim is a progress web app designed for tracking and reporting bikelane hazards, primarily cars.</li>
+                    <li>Lane Claim is built with jQuery, Bootstrap, and SCSS</li>
+                    <li>It uses Firebase <a href="https://firebase.google.com/products/realtime-database/">Realtime Database</a> and <a href="https://firebase.google.com/products/storage/">Storage</a> for its data and uses <a href="https://jestjs.io/">Jest</a> for its unit testing.</li>
+                    </ul>
+                <h4>Complete</h4>
+                <p>See <a href="projects.html">Projects page</a></p>
+            <h3>Experience</h3>
+                <h4><a href="https://www.dentsplysirona.com/en-us">Dentsply Sirona</a>, Waltham, MA</h4>
+                <h5>Production Technician II: (October 2018—Current)</h5>
+                    <ul>
+                    <li>Responsible for receiving, verifying, packaging, and shipping customers' orders at fast pace</li>
+                    <li>Tulip App Administrator</li>
+                    <li>Use 5S, Lean, and Kaizen to increase efficiency and improve work methods</li>
+                    </ul>
+                <h4><a href="https://americanrepertorytheater.org/">American Reperetory Theater - Havard</a>, Cambridge, MA</h4>
+                <h5>Temporary Scene Carpenter/Stagehand: (June 2018—August 2018 )</h5>
+                    <ul>
+                    <li>Built scenery and props for theater productions following blueprints</li>
+                    <li>Loaded and unloaded props, scenery, and tools to and from the Loeb Theater</li>
+                    </ul>
+                <h4><a href="http://billbachantbuilders.com/">Bachant Builders</a>, Wareham, MA</h4>
+                <h5>Foreman/Carpenter: (April 2009—November 2013 / November 2016—January 2018)</h5>
+                    <ul>
+                    <li>Used SketchUp to design blueprints for mapping building dimensions</li>
+                    <li>Had experience doing finish carpentry and installing cabinets</li>
+                    <li>Duties included carpentry/woodworking, automotive repair, masonry, metalworking</li>
+                    </ul>
+                <h4><a href="https://www.facebook.com/Animal-Associates-1455735024727937/">Animal Associates</a>, Dartmouth, MA</h4>
+                <h5>Office Manager/Head Receptionist: (July 2015—November 2016)</h5>
+                    <ul>
+                    <li>In charge of billing, operating the front office, and opening and closing the business each day</li>
+                    <li>Promoted from Head Receptionist to Office Manager in 5 months</li>
+                    <li>Volunteered at “Hearts 4 Paws” animal shelter once a week</li>
+                    </ul>
+                <h4><a href="https://bachantsgunshop.com/">Bachant Gun Shop</a>, Lakeville, MA</h4>
+                <h5>Gunsmith: (October 2014—July 2015)</h5>
+                    <ul>
+                    <li>Designed parts for fabrication using SketchUp CAD software</li>
+                    <li>Cleaned, fixed, and listed firearms for sale on various online platforms</li>
+                    <li>Found creative solutions to mechanical problems without clear instructions</li>
+                    </ul>
+
+            <h3>Education</h3>
+                <h4>Bristol Community College, Fall River, MA (2015—)</h4>
+                <ul>
+                    <li>General Studies</li>
+                </ul>
+                <h4>Apponequet Regional High School, Lakeville, MA (2009—2013)</h4>
+                <ul>
+                    <li>Advance Placement and Honors courses</li>
+                </ul>
+        </div>
+            <hr />
+        <div>
+            <a href="https://docs.google.com/document/d/1d9vJS8ZktSJLMsFDTyhtcAKI29qBRL1bIK5-GjqzBpE/edit?usp=sharing">View Resume on Google Docs</a>
+            <br />
+            <a href="pictures/Garrett-Bachant-Resume.pdf" class="footer" download>Download Resume as PDF</a>
+        </div>   
+    </div>
+  )
+}
+
+export default Resume
