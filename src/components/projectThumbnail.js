50aa7873a96ad4ac68ff2913770e1c5f71989f59
abachant <gbachant@gmail.com>
2020-04-09 17:06:56 -0400

    Convert ProjectThumbnail to a class component

--- src/components/projectThumbnail.js
+++ src/components/projectThumbnail.js
@@ -1,22 +1,25 @@
 import React from 'react';
 
 
-function ProjectThumbnail() {
-  return(
-    <div class="col-md-4">
-        <div class="thumbnail">
-            <a href="">
-                <div>
-                    <img src="logo192.png" alt="thumbnail" />
+class ProjectThumbnail extends React.Component {
+    render() {
+        return(
+            <div class="col-md-4">
+                <div class="thumbnail">
+                    <a href="">
+                        <div>
+                            <img src="logo192.png" alt="thumbnail" />
+                        </div>
+                        <div>
+                            <h3>{this.props.title}</h3>
+                            <p>{this.props.description}</p>
+                        </div>
+                    </a>
                 </div>
-                <div>
-                    <h3>Project Title</h3>
-                    <p>Project Description</p>
-                </div>
-            </a>
-        </div>
-    </div>
-  )
+            </div>
+        );
+            
+    }
 }
 
 export default ProjectThumbnail
--- src/components/projects.js
+++ src/components/projects.js
@@ -5,12 +5,12 @@ import ProjectThumbnail from './projectThumbnail.js';
 function Projects() {
   return(
     <div className="container-fluid">
-      <ProjectThumbnail title="Project 1"/>
-      <ProjectThumbnail title="Project 2"/>
-      <ProjectThumbnail title="Project 3"/>
-      <ProjectThumbnail title="Project 4"/>
-      <ProjectThumbnail title="Project 5"/>
-      <ProjectThumbnail title="Project 6"/>
+      <ProjectThumbnail title="Project 1" description="Project 1 Description"/>
+      <ProjectThumbnail title="Project 2" description="Project 2 Description"/>
+      <ProjectThumbnail title="Project 3" description="Project 3 Description"/>
+      <ProjectThumbnail title="Project 4" description="Project 4 Description"/>
+      <ProjectThumbnail title="Project 5" description="Project 5 Description"/>
+      <ProjectThumbnail title="Project 6" description="Project 6 Description"/>
    </div>
   )
 }
