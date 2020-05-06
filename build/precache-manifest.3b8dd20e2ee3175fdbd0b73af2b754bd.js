b6d9e39c4d4002b49f3eba197d9c86ee43953402
abachant <gbachant@gmail.com>
2020-05-06 15:17:01 -0400

    Deploy :

---
+++ build/precache-manifest.3b8dd20e2ee3175fdbd0b73af2b754bd.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3da90844201959308767be89a9bfc86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dfc25d97661f773c24a",
+    "url": "/static/css/main.2a6e4204.chunk.css"
+  },
+  {
+    "revision": "61d66b72d85e1d142096",
+    "url": "/static/js/2.9be2f9c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9be2f9c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dfc25d97661f773c24a",
+    "url": "/static/js/main.a73105e1.chunk.js"
+  },
+  {
+    "revision": "2c9cc013cafdb4278fcf",
+    "url": "/static/js/runtime-main.0f11ef4a.js"
+  },
+  {
+    "revision": "1948e8c0d203ce9a9016593287a0bbe0",
+    "url": "/static/media/Aubrey_Bachant_Resume.1948e8c0.pdf"
+  },
+  {
+    "revision": "c4c172ec39e008114d9cf0ed5cd27e70",
+    "url": "/static/media/background.c4c172ec.gif"
+  },
+  {
+    "revision": "7eaefe39c199f042dafed03b24e72c3a",
+    "url": "/static/media/instrumental.7eaefe39.png"
+  },
+  {
+    "revision": "a7ae3ae9cb5ca3b77f81d6df413c149b",
+    "url": "/static/media/osmnx_nyc.a7ae3ae9.png"
+  },
+  {
+    "revision": "67777766495f33405dfdd4fafd4d9cd1",
+    "url": "/static/media/osmnx_nyc_closeup.67777766.png"
+  },
+  {
+    "revision": "9d354dac18194a0a87e109a3df8895c2",
+    "url": "/static/media/osmnx_nyc_crop.9d354dac.png"
+  },
+  {
+    "revision": "b5189c3aacb8ea5db944db456752b1b9",
+    "url": "/static/media/osmnx_williamsburg.b5189c3a.png"
+  },
+  {
+    "revision": "95e86c4c3b0e9dd3df98fe73be34cd3d",
+    "url": "/static/media/rhyme_ratio_full.95e86c4c.png"
+  },
+  {
+    "revision": "a0aec0ffe51a5e9cef6a99d965f477f0",
+    "url": "/static/media/rhyme_ratio_mean.a0aec0ff.png"
+  },
+  {
+    "revision": "6bb91319cd32ebb88b27a4c663d7dce6",
+    "url": "/static/media/rhyme_ratio_scatter.6bb91319.png"
+  },
+  {
+    "revision": "392cbe4a8dbafb966ed7427bfd81b6e8",
+    "url": "/static/media/rhyme_ratio_violin.392cbe4a.png"
+  },
+  {
+    "revision": "58a2095c868b081528cfede5c54ceabb",
+    "url": "/static/media/rhyme_ratio_violin_thumbnail.58a2095c.png"
+  },
+  {
+    "revision": "0c08c21db13ffc1177cfca038c1ac176",
+    "url": "/static/media/rhyme_type.0c08c21d.png"
+  },
+  {
+    "revision": "02bae4945c89c1c488839a858e8a56b4",
+    "url": "/static/media/ripta-animation.02bae494.gif"
+  },
+  {
+    "revision": "e012080a4d03d1298a4f71e7b542eb6d",
+    "url": "/static/media/ripta-dashboard.e012080a.png"
+  }
+]);
