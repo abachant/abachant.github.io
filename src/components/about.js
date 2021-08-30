6875e8a23ee44ff595958bdac8ff6b7dbd7adb98
abachant <aubreybachant@gmail.com>
2021-08-30 10:32:51 -0400

    Add basic yaml for building/deploying with github actions

--- src/components/about.js
+++ src/components/about.js
@@ -5,7 +5,7 @@ function About() {
   return(
     <div id="about-background" className="container-fluid">
       <div className="jumbotron">
-        <h1>AUBREY BACHANT</h1>
+        <h1>AUBREY LYNN BACHANT</h1>
         <p>
           I am a programmer and web developer passionate about working with and analyzing
           data to find solutions for civic and social problems. I am particularly interested in applying those skills to help better understand
