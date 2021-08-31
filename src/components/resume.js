814f94f004cc22642f1fc595f139c4c329598253
abachant <aubreybachant@gmail.com>
2021-08-31 18:56:14 -0400

    Update resume

--- src/components/resume.js
+++ src/components/resume.js
@@ -2,118 +2,106 @@ import React from 'react';
 
 
 function Resume() {
-  return(
-    <div className="page-content" id="resume">
-        <div>
-            <h1>AUBREY LYNN BACHANT</h1>
-            <h4><a href="mailto:aubreybachant@gmail.com">aubreybachant@gmail.com</a> | 774-400-8759 | <a href="https://www.linkedin.com/in/aubrey-bachant-36bb77115/">LinkedIn</a> | <a href="https://github.com/abachant">GitHub</a></h4>
-            <h4>Boston, Massachusetts <small>(willing to relocate)</small></h4>
-        </div>
-        <hr />
-        <div>
-            <h3>Skills</h3>
-            <ul>
-                <li>Python</li>
+    return (
+        <div className="page-content" id="resume">
+            <div>
+                <h1>AUBREY LYNN BACHANT</h1>
+                <h4><a href="mailto:aubreybachant@gmail.com">aubreybachant@gmail.com</a> | 774-400-8759 | <a href="https://www.linkedin.com/in/aubrey-bachant/">LinkedIn</a> | <a href="https://github.com/abachant">GitHub</a></h4>
+                <h4>Boston, Massachusetts <small>(willing to relocate)</small></h4>
+            </div>
+            <hr />
+            <div>
+                <h3>Skills</h3>
                 <ul>
-                    <li>Pandas</li>
-                    <li>Jupyter Notebook</li>
-                    <li>Matplotlib</li>
-                    <li>Plotly</li>
+                    <li>JavaScript</li>
+                    <ul>
+                        <li>React</li>
+                        <li>Node</li>
+                        <li>Express</li>
+                        <li>Babel</li>
+                        <li>jQuery</li>
+                        <li>Jest</li>
+                        <li>Sequelize</li>
+                        <li>ES6</li>
+                    </ul>
+                    <li>TypeScript</li>
+                    <li>Python</li>
+                    <ul>
+                        <li>Pandas</li>
+                        <li>Jupyter Notebook</li>
+                        <li>Matplotlib</li>
+                        <li>Plotly</li>
+                    </ul>
+                    <li>SQL</li>
+                    <ul>
+                        <li>PostgreSQL</li>
+                        <li>MariaDB</li>
+                        <li>MYSQL</li>
+                    </ul>
+                    <li>NOSQL</li>
+                    <ul>
+                        <li>Firebase</li>
+                        <li>MongoDB</li>
+                    </ul>
+                    <li>Git</li>
+                    <ul>
+                        <li>GitHub</li>
+                        <li>GitHub Actions</li>
+                    </ul>
+                    <li>Bash</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <ul>
+                        <li>SASS</li>
+                        <li>BEM</li>
+                        <li>Bootstrap</li>
+                    </ul>
+                    <li>Webpack</li>
+                    <li>Docker</li>
+                    <li>Agile Development</li>
+                    <li>Tulip App Builder</li>
+                    <li>Windows Office(Word, Excel, Powerpoint)</li>
+                    <li>SketchUp CAD Software</li>
+                    <li>French(intermediate)</li>
                 </ul>
-                <li>JavaScript</li>
-                <ul>
-                    <li>React</li>
-                    <li>jQuery</li>
-                    <li>Jest</li>
-                </ul>
-                <li>Firebase</li>
-                <li>SQL</li>
-                <li>Git/GitHub</li>
-                <li>Bash</li>
-                <li>HTML</li>
-                <li>CSS</li>
-                <ul>
-                    <li>Bootstrap</li>
-                    <li>SASS</li>
-                    <li>BEM</li>
-                </ul>
-                <li>Tulip App Builder</li>
-                <li>Windows Office(Word, Excel, Powerpoint)</li>
-                <li>SketchUp CAD Software</li>
-                <li>French(intermediate)</li>
-                <li>Reception</li>
-                <li>Construction/Carpentry/Metalworking</li>
-            </ul>
-            <h3>Projects</h3>
-            <div className="resume-subsection">
-                <h4>In Progress</h4>
-                <h5><a href="https://github.com/abachant/Lane-Claim">Lane Claim</a></h5>
+                <h3>Projects</h3>
+                <div className="resume-subsection">
+                    <h4>In Progress</h4>
+                    <h5><a href="https://github.com/abachant/laneclaim">Lane Claim</a></h5>
                     <ul>
                         <li>Lane Claim is a progressive web app designed for tracking and reporting bikelane hazards, primarily cars</li>
-                        <li>It is built in React, styled with SCSS and Bootstrap, and uses Jest for unit testing</li>
+                        <li>It is built in TypeScript and React, is styled with SASS, and uses Jest for testing</li>
                         <li>It uses Firebase <a href="https://firebase.google.com/products/realtime-database/">Realtime Database</a> and <a href="https://firebase.google.com/products/storage/">Storage</a> for its data</li>
                     </ul>
-                <h4>Complete</h4>
-                <p>See <a href="projects">Projects page</a></p>
+                    <h4>Complete</h4>
+                    <p>See <a href="projects">Projects page</a></p>
+                </div>
+                <h3>Experience</h3>
+                <div className="resume-subsection">
+                    <h4><a href="https://www.linkedin.com/company/getturnout/">Everyspace</a>, New York, NY</h4>
+                    <h5>Junior Software Engineer: (June 2021—Current)</h5>
+                    <ul>
+                        <li>Added new reusable React components for searching and adding locations for appointment creation</li>
+                        <li>Added new API endpoints for fetching user information</li>
+                        <li>Increased Unit, Functional, and Integration test coverage by 15%</li>
+                        <li>Used Scrum development to better collaborate with teammates</li>
+                    </ul>
+                </div>
+                <h3>Education</h3>
+                <div className="resume-subsection">
+                    <h4><a href="https://www.hackreactor.com/">Hack Reactor (2021)</a></h4>
+                    <ul>
+                        <li>Advanced Software Engineering Immersive Program</li>
+                        <img className="certificate" src={require("../pictures/Hack_Reactor_Certificate.png")} alt="Hack Reactor Certificate"></img>
+                    </ul>
+                </div>
             </div>
-            <h3>Experience</h3>
-            <div className="resume-subsection">
-                <h4><a href="https://www.dentsplysirona.com/en-us">Dentsply Sirona</a>, Waltham, MA</h4>
-                <h5>Production Technician II: (October 2018—Current)</h5>
-                    <ul>
-                        <li>Used LEAN principles to verify, manufacture, and ship dental implants</li>
-                        <li>Designed and created 5S auditing application on own volition</li>
-                        <li>Tulip App Administrator</li>
-                    </ul>
-                <h4><a href="https://dashride.com">Dashride</a>, New York, NY</h4>
-                <h5>Front End Developer Intern: (June 2018—August 2018 )</h5>
-                    <ul>
-                        <li>Worked on redesign of ride booking tool in jQuery</li>
-                        <li>Worked together with a team of other developers</li>
-                        <li>Used RESTful APIs and Unit Testing</li>
-                    </ul>
-                <h4><a href="http://billbachantbuilders.com/">Bachant Builders</a>, Wareham, MA</h4>
-                <h5>Foreman/Carpenter: (April 2009—November 2013 / November 2016—January 2018)</h5>
-                    <ul>
-                        <li>Used SketchUp to design blueprints for mapping building dimensions</li>
-                        <li>Had experience doing finish carpentry and installing cabinets</li>
-                        <li>Duties included carpentry/woodworking, automotive repair, masonry, metalworking</li>
-                    </ul>
-                <h4><a href="https://www.facebook.com/Animal-Associates-1455735024727937/">Animal Associates</a>, Dartmouth, MA</h4>
-                <h5>Office Manager/Head Receptionist: (July 2015—November 2016)</h5>
-                    <ul>
-                        <li>In charge of billing, operating the front office, and opening and closing the business each day</li>
-                        <li>Promoted from Head Receptionist to Office Manager in 5 months</li>
-                        <li>Volunteered at “Hearts 4 Paws” animal shelter once a week</li>
-                    </ul>
-                <h4><a href="https://bachantsgunshop.com/">Bachant Gun Shop</a>, Lakeville, MA</h4>
-                <h5>Gunsmith: (October 2014—July 2015)</h5>
-                    <ul>
-                        <li>Designed parts for fabrication using SketchUp CAD software</li>
-                        <li>Cleaned, fixed, and listed firearms for sale on various online platforms</li>
-                        <li>Found creative solutions to mechanical problems without clear instructions</li>
-                    </ul>
-            </div>
-            <h3>Education</h3>
-            <div className="resume-subsection">
-                <h4>Bristol Community College, Fall River, MA (2015—)</h4>
-                <ul>
-                    <li>General Studies</li>
-                </ul>
-                <h4>Apponequet Regional High School, Lakeville, MA (2009—2013)</h4>
-                <ul>
-                    <li>Advance Placement and Honors courses</li>
-                </ul>
+            <hr />
+            <div>
+                <a href={require("../pictures/Aubrey_Bachant_Resume.pdf")} download>Download Resume as PDF</a>
             </div>
         </div>
-        <hr />
-        <div>
-            <a href="https://docs.google.com/document/d/1L6P6jkl4q3_AXfLKtrq0fPq4rQqa8FdX_cPikwOKdus/edit?usp=sharing">View Resume on Google Docs</a>
-            <br />
-            <a href={require("../pictures/Aubrey_Bachant_Resume.pdf")} download>Download Resume as PDF</a>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Resume
